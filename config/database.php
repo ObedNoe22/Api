01c128fdaea1339e790a83dd124de0118b494b0a
Obed Martinez <obednoe22@gmail.com>
2020-05-19 20:49:33 -0500

    Pgsql

--- config/database.php
+++ config/database.php
@@ -58,8 +58,8 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => $DATABASE_URL["host"],
-            'port' => $DATABASE_URL["port"],
-            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'port' => $DATABASE_URL["host"],
+            'database' => ltrim($DATABASE_URL["path"],"/"),
             'username' => $DATABASE_URL["user"],
             'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
